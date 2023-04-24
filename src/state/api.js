d6b39f2fc8e6cbae388adca7f4d9dc872331db8e
thards89 <thards89@gmail.com>
2023-04-24 20:54:46 +0200

    performance and dashboard pages done

--- src/state/api.js
+++ src/state/api.js
@@ -1,16 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-    reducerPath: "adminApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
   tagTypes: [
-    "User", 
+    "User",
     "Products",
     "Customers",
     "Transactions",
     "Geography",
     "Sales",
-    "Admins"
+    "Admins",
+    "Performance",
+    "Dashboard",
   ],
   endpoints: (build) => ({
     getUser: build.query({
@@ -45,15 +47,25 @@ export const api = createApi({
       query: () => "management/admins",
       providesTags: ["Admins"],
     }),
-  })
-})
+    getUserPerformance: build.query({
+      query: (id) => `management/performance/${id}`,
+      providesTags: ["Performance"],
+    }),
+    getDashboard: build.query({
+      query: () => "general/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
+});
 
 export const {
-    useGetUserQuery,
-    useGetProductsQuery,
-    useGetCustomersQuery,
-    useGetTransactionsQuery,
-    useGetGeographyQuery,
-    useGetSalesQuery,
-    useGetAdminsQuery
-  } = api;
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetUserPerformanceQuery,
+  useGetDashboardQuery,
+} = api;
